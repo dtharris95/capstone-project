6a82ae936999c9f558559ea43e2edac0fb93056e
dtharris95 <72363146+dtharris95@users.noreply.github.com>
2023-06-10 10:19:16 -0400

    Add files via upload

---
+++ capstone.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#index").html("Vinyl Finder")
+
+    $("#homeintro").html("For fans of vinyl records.")
+
+    $("#about").html("About")
+
+    $("#contact").html("Contact")
+
+    $("#search").html("Search")
+
+    $("#searchselecttab").html("Use one of the three tabs below to start searching!")
+
+    $("#browseartist").html("Look up an artist below to view their discography and find which of their records are available.")
+
+    $("#browsealbum").html("Look up a specific album below to check on its price and availability.")
+
+    $("#sitepurpose").html("About Me")
+
+    $("#paraabout").html("This website was designed with the record collector in mind!<br><br>I began my record collection in 2021, being gifted two records for Christmas - 'Experience Hendrix: The Best of Jimi Hendrix', and 'Camp' by Childish Gambino. Not long after, I began searching for copies of albums that I liked! Imagine my surprise when I find that there is no easy way to search and quickly be able to compare pricing and availability between various websites. I wanted to simplify the vinyl record search process.<br><br>Whether searching for your first record or adding to your extensive collection, this website was created with the intention of making the search for some of those hard to find records easier.")
+
+    $("#paracontact").html("I hope that this website experience meets and exceeds your expectations.<br><br>For any questions, please be sure to fill out the form below.")
+
+});
